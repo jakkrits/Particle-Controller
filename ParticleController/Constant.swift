07bdb523360bea2ad25964cfdc779dab2b69ee5a
Jakk Sut <jakks@Jakks-iMac.local>
2017-09-05 00:34:30 +0700

    ‘init’

---
+++ ParticleController/Constant.swift
@@ -0,0 +1,30 @@
+//
+//  Constant.swift
+//  iParticle
+//
+//  Created by JakkritS on 3/14/2559 BE.
+//  Copyright © 2559 AppIllustrator. All rights reserved.
+//
+
+struct StoryboardID {
+    static let DeviceDetailTableViewController = "DeviceDetailTableViewControllerID"
+    static let MainTableViewController = "MainTableViewControllerID"
+    static let ResultViewController = "ResultViewControllerID"
+}
+
+struct SegueID {
+    static let MainToDeviceDetailSegue = "MainToDeviceDetailSegue"
+    static let DeviceDetailToResultSegue = "DeviceDetailToResultSegue"
+}
+
+struct CellID {
+    static let MainCellID = "MainViewCellID"
+    static let SetupCellID = "SetupCellID"
+    static let DetailCellID = "DetailCellID"
+}
+
+struct ThemeColor {
+    static let GreenColor = UIColor(red:0.57, green:0.86, blue:0.35, alpha:1)
+    static let BlueColor = UIColor(red:0.27, green:0.54, blue:0.99, alpha:1)
+    static let GrayColor = UIColor(red:0.86, green:0.86, blue:0.86, alpha:1)
+}
